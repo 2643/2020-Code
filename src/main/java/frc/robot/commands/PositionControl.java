8b57269d25c865cb4788d1e5ce32c6bf605624a5
Sanjana679 <sanjana@nandi.net>
2020-02-12 16:45:10 -0800

    Added two files

--- src/main/java/frc/robot/commands/PositionControl.java
+++ src/main/java/frc/robot/commands/PositionControl.java
@@ -4,21 +4,17 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-//turns the wheel 7 times over a singular color
 
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
-public class RotationControl extends CommandBase {
-  int x=0;
-
+public class PositionControl extends CommandBase {
   /**
-   * Creates a new RotationControl
+   * Creates a new PositionControl.
    */
-  public RotationControl() {
+  public PositionControl() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(RobotContainer.frictionWheel);
   }
@@ -26,34 +22,21 @@ public class RotationControl extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    //Starting the counter
-    x=0;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    //make the control panel pass 7 times over Green
-    if(x<7){
-      if(RobotContainer.frictionWheel.getColor().equals("Green")){
-        x+=1;
-      }
-      RobotContainer.frictionWheel.setMotorSpeed(Constants.frictionWheelSpeed);
-    } 
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    RobotContainer.frictionWheel.setMotorSpeed(0);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (x==10){
-      return true;
-    }
     return false;
   }
 }
