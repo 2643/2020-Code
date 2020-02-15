f732885ce66ab553a3bbc306fcff2a83a540b08b
Navneeth5432 <navneeth.suresh@gmail.com>
2020-02-14 16:18:49 -0800

    Button Mappings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
@@ -40,10 +41,24 @@ public class RobotContainer {
   public static Joystick driveStick = new Joystick(0);
   public static Joystick opBoard = new Joystick(1);
 
+  public static JoystickButton forwardConveyor = new JoystickButton(opBoard, 1); //TODO change this to correct button
+  public static JoystickButton reverseConveyor = new JoystickButton(opBoard, 2); //TODO change this to correct button
+  public static JoystickButton manualIntake = new JoystickButton(opBoard, 3); //TODO change this to correct button
+  public static JoystickButton reverseIntake = new JoystickButton(opBoard, 4); //TODO change this to correct button
+  public static JoystickButton manualControlPanel = new JoystickButton(opBoard, 5); //TODO change this to correct button
+  public static JoystickButton autoIntake = new JoystickButton(opBoard, 6); //TODO change this to correct button
+  public static JoystickButton rotationControl = new JoystickButton(opBoard, 7); //TODO change this to correct button
+  public static JoystickButton positionControl = new JoystickButton(opBoard, 8); //TODO change this to correct button
+  public static JoystickButton autoShoot = new JoystickButton(opBoard, 9); //TODO change this to correct button
+  public static JoystickButton manualShooting = new JoystickButton(opBoard, 10); //TODO change this to correct button
+  public static JoystickButton hookDelivery = new JoystickButton(opBoard, 11); //TODO change this to correct button
+  public static JoystickButton dropTelescope = new JoystickButton(opBoard, 12); //TODO change this to correct button
+  public static JoystickButton leftClimb = new JoystickButton(opBoard, 13); //TODO change this to correct button
+  public static JoystickButton rightClimb = new JoystickButton(opBoard, 14); //TODO change this to correct button
+  public static JoystickButton bothWinchClimb = new JoystickButton(opBoard, 15); //TODO change this to correct button
   
-
-
-  
+  public static JoystickButton controlPanel = new JoystickButton(driveStick, 5);
+  public static JoystickButton verticalIntake = new JoystickButton(driveStick, 6);
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
@@ -59,9 +74,17 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
+    forwardConveyor.whileHeld(new ForwardConveyor());
+    reverseConveyor.whileHeld(new ReverseConveyor());
+    manualIntake.whileHeld(new ForwardIntake());
+    reverseIntake.whileHeld(new ReverseIntake());
 
-
+    autoIntake.whenPressed(new Intaking());
+    rotationControl.whenPressed(new RotationControl()); //TODO check whether it is whenPressed or something else
+    positionControl.whileHeld(new PositionControl()); //TODO check whether it is whileHeld or something else
     
+
+
   }
 
 
