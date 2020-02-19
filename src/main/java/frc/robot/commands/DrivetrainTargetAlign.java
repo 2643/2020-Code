fe419361bcb5194e76fce2be9245275dda9393f2
GaneshPimpale <pimpale2003@gmail.com>
2020-02-19 10:14:45 -0800

    im really stupid if you have not yet noticed

--- src/main/java/frc/robot/commands/DrivetrainTargetAlign.java
+++ src/main/java/frc/robot/commands/DrivetrainTargetAlign.java
@@ -1,4 +1,4 @@
-import frc.robot.commands;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
@@ -22,25 +22,25 @@ public class DrivetrainTargetAlign extends CommandBase {
 
     System.out.println("Turn left: "+movement[0]+", Turn right: "+movement[1]+", Move Back: "+movement[2]+", Move Forwards: "+movement[3]);
 
-    if (movement_array[0] == true){
+    if (movement[0] == true){
       // Turn Left
       RobotContainer.drivetrain.setLeftMotorSpeed(-0.3);
       RobotContainer.drivetrain.setRightMotorSpeed(0.3);
     }
 
-    if (movement_array[1] == true){
+    if (movement[1] == true){
       // Turn Right
       RobotContainer.drivetrain.setLeftMotorSpeed(0.3);
       RobotContainer.drivetrain.setRightMotorSpeed(-0.3);
     }
 
-    if (movement_array[2] == true){
+    if (movement[2] == true){
       // Move backwards
       RobotContainer.drivetrain.setLeftMotorSpeed(-0.3);
       RobotContainer.drivetrain.setRightMotorSpeed(-0.3);
     }
 
-    if (movement_array[3] == true){
+    if (movement[3] == true){
       // Move forwards
       RobotContainer.drivetrain.setLeftMotorSpeed(0.3);
       RobotContainer.drivetrain.setRightMotorSpeed(0.3);
