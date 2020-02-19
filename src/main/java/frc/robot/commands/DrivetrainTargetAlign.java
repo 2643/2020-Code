8babd661939e2be76d1b11a83efc5c724cd0f387
GaneshPimpale <pimpale2003@gmail.com>
2020-02-19 00:07:27 -0800

    fixed errors??? probably more

--- src/main/java/frc/robot/commands/DrivetrainTargetAlign.java
+++ src/main/java/frc/robot/commands/DrivetrainTargetAlign.java
@@ -1,56 +1,70 @@
 import frc.robot.commands.*;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 import frc.robot.Constants;
 
-public class DrivetrainTargetAlign {
+public class DrivetrainTargetAlign extends CommandBase {
+  
+  private boolean finished = false;
 
   public DrivetrainTargetAlign() {
-    requires(Robot.drive);
+    addRequirements(RobotContainer.drivetrain);
+  }
+  
+  @Override
+  public void initialize() {
   }
 
   @Override
   protected void execute() {
     boolean [] movement = RobotMap.visionTable.getEntry("movement_array");
+
     System.out.println("Turn left: "+movement[0]+", Turn right: "+movement[1]+", Move Back: "+movement[2]+", Move Forwards: "+movement[3]);
 
-    if (haveToMove == true) {
-      if (movement_array[0] == true){
-        RotateX(5);
-      }
-
-      if (movement_array[1] == true){
-        RotateX(-5);
-      }
-
-      if (movement_array[2] == true){
-
-      }
-
-      if (movement_array[3] == true){
-
-      }
+    if (movement_array[0] == true){
+      // Turn Left
+      RobotContainer.drivetrain.setLeftMotorSpeed(-0.3);
+      RobotContainer.drivetrain.setRightMotorSpeed(0.3);
     }
+
+    if (movement_array[1] == true){
+      // Turn Right
+      RobotContainer.drivetrain.setLeftMotorSpeed(0.3);
+      RobotContainer.drivetrain.setRightMotorSpeed(-0.3);
+    }
+
+    if (movement_array[2] == true){
+      // Move backwards
+      RobotContainer.drivetrain.setLeftMotorSpeed(-0.3);
+      RobotContainer.drivetrain.setRightMotorSpeed(-0.3);
+    }
+
+    if (movement_array[3] == true){
+      // Move forwards
+      RobotContainer.drivetrain.setLeftMotorSpeed(0.3);
+      RobotContainer.drivetrain.setRightMotorSpeed(0.3);
+    }
+
+    finished = true;
   }
 
   @Override
-  protected void end() {
-    Robot.drive.setAllSpeed(0, 0);
-  }
-
-  @Override
-  protected void interrupted() {
-    end();
+  protected void end(boolean interrupted) {
+    RobotContainer.drivetrain.setLeftMotorSpeed(0);
+    RobotContainer.drivetrain.setRightMotorSpeed(0);
   }
 
   @Override
   protected boolean isFinished() {
-    return Robot.drive.isStuck();
+    if (finished == true) {
+    return true;
+    }
+
+    else {
+      return false;
+    }
   }
 }
-
-// TODO Literlly write all the code, DW it will get done...
-
 
 
