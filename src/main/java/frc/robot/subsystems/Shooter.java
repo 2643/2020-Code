6aac1888b267b931ddcf17ae7b225060252d0d82
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2020-02-21 15:36:08 -0800

    Shooter updated

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,66 +1,94 @@
-// /*----------------------------------------------------------------------------*/
-// /* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-// /* Open Source Software - may be modified and shared by FRC teams. The code   */
-// /* must be accompanied by the FIRST BSD license file in the root directory of */
-// /* the project.                                                               */
-// /*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
-// package frc.robot.subsystems;
+package frc.robot.subsystems;
 
-// import com.revrobotics.CANSparkMax;
-// import com.revrobotics.ControlType;
-// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import frc.robot.Constants;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-// TODO uncomment when shooter is added to the robot
-// public class Shooter extends SubsystemBase {
-//   public static CANSparkMax leftShooterMotor = new CANSparkMax(Constants.leftShooterMotorPort, MotorType.kBrushless);
-//   public static CANSparkMax rightShooterMotor = new CANSparkMax(Constants.rightShooterMotorPort, MotorType.kBrushless);
+public class Shooter extends SubsystemBase {
+  static CANSparkMax m_motorOne = new CANSparkMax(21, MotorType.kBrushless);
+  static CANSparkMax m_motorTwo = new CANSparkMax(20, MotorType.kBrushless);
+  static CANEncoder m_encoderOne;
+  static CANEncoder m_encoderTwo;
+  private static CANPIDController m_pidControllerOne;
+  private static CANPIDController m_pidControllerTwo;
 
-//   //Shooter PID Constants
-//   private static final double kP = 0.00015;
-//   private static final double kI = 0;
-//   private static final double kD = 0;
-//   private static final double kIz = 0;
-//   private static final double kFF = 0.000015;
-//   private static final double kMaxOutput = 1;
-//   private static final double kMinOutput = -1;
-//   private static final double maxRPM = 5700;
-  
-//   private static int slotID = 0; 
-//   /**
-//    * Creates a new Shooter.
-//    */
-//   public Shooter() {
-//     leftShooterMotor.getPIDController().setP(kP, slotID);
-//     leftShooterMotor.getPIDController().setI(kI, slotID);
-//     leftShooterMotor.getPIDController().setD(kD, slotID);
-//     leftShooterMotor.getPIDController().setIZone(kIz, slotID);
-//     leftShooterMotor.getPIDController().setFF(kFF, slotID);
-//     leftShooterMotor.getPIDController().setOutputRange(kMinOutput, kMaxOutput, slotID);
+  /**
+   * Creates a new Shooter.
+   */
+  public Shooter() {
+    final double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM;
 
-//     rightShooterMotor.getPIDController().setP(kP, slotID);
-//     rightShooterMotor.getPIDController().setI(kI, slotID);
-//     rightShooterMotor.getPIDController().setD(kD, slotID);
-//     rightShooterMotor.getPIDController().setIZone(kIz, slotID);
-//     rightShooterMotor.getPIDController().setFF(kFF, slotID);
-//     rightShooterMotor.getPIDController().setOutputRange(kMinOutput, kMaxOutput, slotID);
-//   }
+    // initialize motor
+    /**
+     * In order to use PID functionality for a controller, a CANPIDController object
+     * is constructed by calling the getPIDController() method on an existing
+     * CANSparkMax object
+     */
 
-//   /**
-//    * Spins the motor at a given speed in RPM
-//    * @param speed RPM to spin motor at
-//    */
-//   public void spinMotors(double speed){
-//     leftShooterMotor.getPIDController().setReference(speed, ControlType.kVelocity);
-//     rightShooterMotor.getPIDController().setReference(-speed, ControlType.kVelocity);
-//   }
+    m_pidControllerOne = m_motorOne.getPIDController();
+    m_pidControllerTwo = m_motorTwo.getPIDController();
+
+    // Encoder object created to display position values
+    m_encoderOne = m_motorOne.getEncoder();
+    m_encoderTwo = m_motorTwo.getEncoder();
+
+    // PID coefficients
+    kP = 0.00067; //0.00015
+    kI = 0.000001;
+    kD = 0.0002;
+    kIz = 100;
+    kFF = 0.0002;
+    kMaxOutput = 0.7;
+    kMinOutput = -0.7;
+    maxRPM = 5700;
 
 
-//   @Override
-//   public void periodic() {
-//     // This method will be called once per scheduler run
-//   }
-// }
+    // set PID coefficients
+    m_pidControllerOne.setP(kP);
+    m_pidControllerOne.setI(kI);
+    m_pidControllerOne.setD(kD);
+    m_pidControllerOne.setIZone(kIz);
+    m_pidControllerOne.setFF(kFF);
+    m_pidControllerOne.setOutputRange(kMinOutput, kMaxOutput);
+
+    m_pidControllerTwo.setP(kP);
+    m_pidControllerTwo.setI(kI);
+    m_pidControllerTwo.setD(kD);
+    m_pidControllerTwo.setIZone(kIz);
+    m_pidControllerTwo.setFF(kFF);
+    m_pidControllerTwo.setOutputRange(kMinOutput, kMaxOutput);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    System.out.print(m_encoderOne.getVelocity()+"  ");
+    System.out.println(m_encoderTwo.getVelocity());
+  }
+
+  public static void stop() {
+    m_pidControllerOne.setReference(0, ControlType.kDutyCycle);
+    m_pidControllerTwo.setReference(0, ControlType.kDutyCycle);
+  }
+
+  public static void speedUp(int speed) {
+    m_pidControllerOne.setReference(speed, ControlType.kVelocity);
+    m_pidControllerTwo.setReference(-speed, ControlType.kVelocity);
+  }
+
+  public static void setSpeed(double speed) {
+    m_motorOne.set(speed);
+    m_motorTwo.set(-speed);
+  }
+}
