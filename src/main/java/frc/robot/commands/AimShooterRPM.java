33ec5e39f12329f9c92dfde2a7e43ff84e7ad14b
GaneshPimpale <pimpale2003@gmail.com>
2020-02-25 16:24:13 -0800

    yyet

--- src/main/java/frc/robot/commands/AimShooterRPM.java
+++ src/main/java/frc/robot/commands/AimShooterRPM.java
@@ -8,11 +8,10 @@
  public class AimShooterRPM extends CommandBase {
 
      private boolean finished = false;
-     private int shooterRPM;
-     private double XfromTarget = RobotContainer.tfmini.getDistance();
 
      public AimShooterRPM() {
          addRequirements(RobotContainer.shooter);
+         addRequirements(RobotContainer.hood);
          addRequirements(RobotContainer.tfmini);
      }
 
@@ -22,16 +21,33 @@
 
      @Override
      public void execute() {
+         // Find distance from the target
+         private double XfromTarget = RobotContainer.tfmini.getDistance();
+
          // Compute shooter RPM
          // Linear function:
          //double MaxshooterRPM = (1.14 * XfromTarget) + 1513;
          //double MinshooterRPM = (1.27 * XfromTarget) + 1074;
+
          // Exponential function:
          double MaxshooterRPM = 1577*(Math.exp(5.25*(10**-4)*XfromTarget));
          double MinshooterRPM = 1160*(Math.exp(7.11*(10**-4)*XfromTarget));
-         shooterRPM = (MaxshooterRPM + MinshooterRPM) / 2;
+         double shooterRPM = (MaxshooterRPM + MinshooterRPM) / 2;
 
+         // Compute hood angle value
+         // logorithmic function
+         double hoodRotation = -15.7 + 5.71*Math.log(XfromTarget);
 
+         // Convert rotation to encoder ticks (42 ticks per rotation)
+         double hoodAngle = (hoodRotation * 42);
+         if (num % 5 < 2.5) {
+             hoodAngle = hoodAngle - hoodAngle % 5;
+         }
+         else if (num % 5 > 2.5)
+             hoodAngle = hoodAngle + (5 - hoodAngle % 5);
+
+         // Shoot Bruh
+         RobotContainer.hood.moveHood(hoodAngle);
          RobotContainer.shooter.spinMotors(shooterRPM);
          finished = true;
      }
